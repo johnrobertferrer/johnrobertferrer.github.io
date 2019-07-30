daccc74d4f5c00161dab6912c8280275bdae1a87
johnrobert_nelsoft <johnrobert.ferrer.nelsoft@gmail.com>
2019-07-30 18:41:39 +0800

    :tada: initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <core-toolbar />
+
+    <core-drawer />
+
+    <core-view />
+
+    <core-footer />
+
+    <core-cta />
+  </v-app>
+</template>
+
+<script>
+
+  export default {
+    name: 'App',
+    components: {
+      CoreCta: () => import('@/components/core/Cta'),
+      CoreDrawer: () => import('@/components/core/Drawer'),
+      CoreFooter: () => import('@/components/core/Footer'),
+      CoreToolbar: () => import('@/components/core/Toolbar'),
+      CoreView: () => import('@/components/core/View')
+    },
+    data () {
+      return {
+      //
+      }
+    }
+  }
+</script>
