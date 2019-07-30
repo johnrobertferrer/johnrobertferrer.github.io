ee4358eaeca6af6ceb551e58e68f241976f1a644
johnrobert_nelsoft <johnrobert.ferrer.nelsoft@gmail.com>
2019-07-30 19:09:35 +0800

    Initial dist subtree commit

---
+++ dist/js/chunk-2d0f0100.8372ad0e.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0f0100"],{"9b89":function(e,n,t){"use strict";t.r(n);var u=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("section",{attrs:{id:"feed"}},[t("feed",[e._t("default")],2)],1)},c=[],l={components:{Feed:function(){return Promise.all([t.e("chunk-53c6b3a9"),t.e("chunk-25ddaf33")]).then(t.bind(null,"f5bd"))}}},d=l,a=t("2877"),o=Object(a["a"])(d,u,c,!1,null,null,null);n["default"]=o.exports}}]);
+//# sourceMappingURL=chunk-2d0f0100.8372ad0e.js.map
